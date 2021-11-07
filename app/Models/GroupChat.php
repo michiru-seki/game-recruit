7378bcfc16da7ac9346cb88c3e4674963e764d96
seki <okiu_soccer7@icloud.com>
2021-11-07 17:56:01 +0900

    モデルファイル作成

---
+++ app/Models/GroupChat.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupChat extends Model
+{
+    use HasFactory;
+
+    protected $table = 'group_chat';
+
+    protected $fillable = [
+        'group_id',
+        'user_id',
+        'message',
+        'image',
+        'movie',
+        'read_status',
+    ];
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,36 +5,18 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table = 'users';
+
     protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'user_name',
+        'password',
+        'icon',
+        'game',
+        'introduction',
     ];
 }
